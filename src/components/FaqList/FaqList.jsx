89eb80941d371dc2afb8007f748b7d27dc7cb198
Noman <pxycknomdictator@outlook.com>
2024-05-04 21:11:33 +0500

    Project Completed :)

--- src/components/FaqList/FaqList.jsx
+++ src/components/FaqList/FaqList.jsx
@@ -1,24 +1,27 @@
-import React from "react";
-import plusIcon from "../../assets/icon-plus.svg"
-import minusIcon from "../../assets/icon-minus.svg"
+import React, { useState } from "react";
+import plusIcon from "../../assets/icon-plus.svg";
+import minusIcon from "../../assets/icon-minus.svg";
 import "../../index.css";
 import "./FaqList.css";
 
 const FaqList = ({ faqs }) => {
-  
+  const [isActive, setIsActive] = useState(false);
+
+  const EventToggler = () => {
+    setIsActive((preActive) => !preActive);
+  };
+
   const { title, answer } = faqs;
 
   return (
     <>
       <div className="faqlist-body">
-        <div className="question flex items-center">
+        <div className="question flex items-center" onClick={EventToggler}>
           <p className="bold qna">{title}</p>
-          <img className="icons" src={plusIcon} alt="plusIcon" />
+          <img className="icons" src={isActive ? minusIcon : plusIcon} alt="plusIcon" />
         </div>
-        <div className="answer deactive">
-          <p>
-            {answer}
-          </p>
+        <div className={`answer ${isActive ? "active" : "deactive"}`}>
+          <p>{answer}</p>
         </div>
       </div>
     </>
