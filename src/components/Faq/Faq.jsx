f558c2d58da62175791ff670643fb2110f19d103
Noman <pxycknomdictator@outlook.com>
2024-05-04 18:24:31 +0500

    Maked fully responsive desing only

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import FaqList from "../FaqList/FaqList";
 import faq from "../../data/faq.json";
+import startIcon from "../../assets/icon-star.svg";
 import "../../index.css";
 import "./Faq.css";
 
 const Faq = () => {
-  console.log(faq);
   return (
     <>
-      <h1>FAQ Body</h1>
-      <FaqList />
+      <div className="faqBody">
+        <div className="accordion-heading flex items-center mx-auto">
+          <img src={startIcon} alt="starIcon" />
+          <h1>FAQ</h1>
+        </div>
+
+        {faq.map((elements, index) => (
+          <FaqList key={index} faqs={elements} />
+        ))}
+      </div>
     </>
   );
 };
--- src/components/FaqList/FaqList.jsx
+++ src/components/FaqList/FaqList.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import "../../index.css"
-import "./FaqList.css"
+import React from "react";
+import plusIcon from "../../assets/icon-plus.svg"
+import minusIcon from "../../assets/icon-minus.svg"
+import "../../index.css";
+import "./FaqList.css";
 
-const FaqList = () => {
+const FaqList = ({ faqs }) => {
+  
+  const { title, answer } = faqs;
+
   return (
-    <div>FaqList</div>
-  )
-}
+    <>
+      <div className="faqlist-body">
+        <div className="question flex items-center">
+          <p className="bold qna">{title}</p>
+          <img className="icons" src={plusIcon} alt="plusIcon" />
+        </div>
+        <div className="answer deactive">
+          <p>
+            {answer}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default FaqList
+export default FaqList;
